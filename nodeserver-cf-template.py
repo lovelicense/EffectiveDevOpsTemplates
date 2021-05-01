e8c8e8dab6d6347f6247398789fe41fecfa9a795
park jae kyu <parkjaekyu21@gmail.com>
2021-05-01 14:13:34 +0900

    Allowing the instance to describe the ASG instances

--- nodeserver-cf-template.py
+++ nodeserver-cf-template.py
@@ -205,6 +205,7 @@ t.add_resource(IAMPolicy(
                                             Action("logs", "Describe*"),
                                             Action("events", "Put*"),
                                             Action("firehose", "Put*"),
+                                            Action("autoscaling", "DescribeAutoScalingInstances"),
                                             ],
                                        Resource=["*"])
                                  ]
