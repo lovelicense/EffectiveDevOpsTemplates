9769a2787e33d9d3b6cd7806e49ef46ee190024f
park jae kyu <parkjaekyu21@gmail.com>
2021-05-01 12:57:34 +0900

    Allowing our application to send logs to Firehose

--- nodeserver-cf-template.py
+++ nodeserver-cf-template.py
@@ -204,6 +204,7 @@ t.add_resource(IAMPolicy(
                                             Action("logs", "Put*"),
                                             Action("logs", "Describe*"),
                                             Action("events", "Put*"),
+                                            Action("firehose", "Put*"),
                                             ],
                                        Resource=["*"])
                                  ]
